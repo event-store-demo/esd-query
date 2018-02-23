415338d6e8402aca22bb1523645afe542eb1658d
Daniel Frey <dmfrey@gmail.com>
2018-02-23 15:47:42 -0500

    add event store implementation

---
+++ src/test/java/io/pivotal/dmfrey/eventStoreDemo/config/UnitTestConfig.java
@@ -0,0 +1,30 @@
+package io.pivotal.dmfrey.eventStoreDemo.config;
+
+import io.pivotal.dmfrey.eventStoreDemo.domain.client.BoardClient;
+import io.pivotal.dmfrey.eventStoreDemo.domain.model.Board;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+
+import java.util.UUID;
+
+@TestConfiguration
+public class UnitTestConfig {
+
+    @Bean
+    @Primary
+    public BoardClient boardClient() {
+
+        return new BoardClient() {
+
+            @Override
+            public Board find( UUID boardUuid ) {
+
+                throw new UnsupportedOperationException( "client call not implemented yet" );
+            }
+
+        };
+
+    }
+
+}
